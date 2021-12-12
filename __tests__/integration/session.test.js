747a2cf4dee0dc5a679f15aa6ee5429e78d00379
José Jonathan <josejonathan689@gmail.com>
2021-12-12 15:24:48 -0300

    fim da aula

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,13 +1,72 @@
-const { User } = require("../../src/app/models");
-const { describe, it, expect } = require("@jest/globals");
+const request = require("supertest");
+const { describe, it, expect, beforeEach } = require("@jest/globals");
+const app = require("../../src/app");
+const truncate = require("../utils/truncate");
+const  factory  = require("../factory");
 
 describe("#Authenticate", () => {
+	beforeEach(async () => {
+		await truncate();
+	});
 
-	it("->Should sum two numbers", async () => {
-		const user = await User.create({ name: "jose", email: "jonahtan@gmail.com", password_hash: "asdasda"});
-		console.log(user);
+	it("->Should authenticate with valid credentials", async () => {
+		const user = await factory.create("User", {
+			password: "123123"
+		});
 
-		expect(user.name).toBe("jose");
+		const response = await request(app).post("/sessions").send({
+			email: user.email,
+			password: "123123"
+		});
 
+		expect(response.status).toBe(200);
+	});
+
+	it("->Should not authenticate with invalid creadentials", async () => {
+		const user = await factory.create("User");
+
+		const response = await request(app).post("/sessions").send({
+			email: user.email,
+			password: "123457"
+		});
+
+		expect(response.status).toBe(401);
+	});
+
+	it("->Should return JWT token when authenticated", async () => {
+		const user = await factory.create("User",{
+			password: "123456"
+		});
+
+		const response = await request(app).post("/sessions").send({
+			email: user.email,
+			password: "123456"
+		});
+
+		expect(response.body).toHaveProperty("token");
+	});
+
+	it("->Should be able access private routes when authenticated", async () => {
+		const user = await factory.create("User",{
+			password: "123456"
+		});
+
+		const response = await request(app).get("/dashboard").set("Authorization", `Bearer ${user.generateToken()}`);
+
+		expect(response.status).toBe(200);
+	});
+
+	it("->Should not be able access private routes whitout JWT token", async () => {
+
+		const response = await request(app).get("/dashboard");
+
+		expect(response.status).toBe(401);
+	});
+
+	it("->Should not be able access private routes with invalid JWT token", async () => {
+
+		const response = await request(app).get("/dashboard").set("Authorization", "Bearer 123123");
+
+		expect(response.status).toBe(401);
 	});
 });
---
+++ __tests__/unit/User.test.js
@@ -0,0 +1,24 @@
+const { describe, it, beforeEach, expect } = require("@jest/globals");
+const bcrypt = require("bcryptjs");
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("#User", () => {
+	beforeEach( async () => {
+		await truncate();
+	});
+
+	it("->Should encrypt user password", async () => {
+		const user = await User.create({
+			name: "jose",
+			email: "jonahtan@gmail.com",
+			password: "123456"
+		});
+
+		const compareHash = await bcrypt.compare("123456", user.password_hash);
+
+		expect(compareHash).toBe(true);
+
+
+	});
+});
